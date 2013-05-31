4632ed55b1b233175fa08dceb74938d559160dc5
Raynos <raynos2@gmail.com>
2013-05-30 23:49:24 -0700

    put functionality back

--- templates/main.js
+++ templates/main.js
@@ -4,18 +4,24 @@ module.exports = Main
 
 function Main(profiles) {
     return ["body.main", [
-        [".sidebar", [
+        [".sidebar", { "data-marker": "profiles" }, [
             [".slider"],
             [".header", [
                 ["h2.profile-header", "Profiles"]
             ]],
             [".profiles", [
-                ["ul", profiles.map(profileItem)]
+                ["ul", {
+                    "data-marker": "profilesList"
+                }, profiles.map(profileItem)]
             ]],
             [".footer", [
                 ["button", {
                     "data-marker": "addProfile"
-                }, "Add Profile"]
+                }, "Add Profile"],
+                ["input", {
+                    "data-marker": "profileName",
+                    "placeholder": "Name your new profile"
+                }]
             ]]
         ]],
         ["script", { src: "/js/main" }]

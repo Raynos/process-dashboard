5dabe069420b8904889bd3e4de1755ec07896c36
Raynos <raynos2@gmail.com>
2013-05-31 00:23:41 -0700

    fix &nbsp; thing. Fix styling a little

--- templates/profile.js
+++ templates/profile.js
@@ -1,11 +1,10 @@
 module.exports = profileItem
 
 function profileItem(profile) {
-    return ["li", {
+    return ["li.profile-item", {
         "data-marker": "profiles." + profile.name
     }, [
-        ["span", profile.name],
-        ["span", "&nbsp;"],
+        ["span.profile-name", profile.name],
         ["a", { href: "#/profiles/" + profile.name + "/delete" }, "X"]
     ]]
 }

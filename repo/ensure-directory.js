6704e3226fc1344c6260b64d62418bdc93c78951
Raynos <raynos2@gmail.com>
2013-06-11 22:57:18 -0700

    fixed ensure-directory

--- repo/ensure-directory.js
+++ repo/ensure-directory.js
@@ -1,60 +1,33 @@
 var fs = require("fs")
 var path = require("path")
-var console = require("console")
-var process = require("process")
-var env = process.env
-
-var chain = require("continuable/chain")
-var map = require("continuable/map")
-var of = require("continuable/of")
-var either = require("continuable/either")
+var env = require("process").env
+var async = require("gens")
+var both = require("gens/both")
 
 // ensureDirectory := () => Continuable<String>
 // ensureDirectory returns the directory where profiles are stored
-module.exports = ensureDirectory
+module.exports = async(ensureDirectory)
 
-function ensureDirectory() {
-    var homeDir = home()
-    var configDir = path.join(homeDir, ".config")
-    var configDirStat = fs.stat.bind(null, configDir)
+function* ensureDirectory() {
+    var configDir = path.join(home(), ".config")
+    var configDirStat = yield both(fs.stat.bind(null, configDir))
 
-    var configDirExists = either(configDirStat, function () {
-        return fs.mkdir.bind(null, configDir)
-    }, function (stat) {
-        if (stat && !stat.isDirectory()) {
-            console.error("~/.config directory exists but " +
-                "it is not a directory")
-            return process.exit(1)
-        }
+    if (configDirStat[0]) {
+        yield fs.mkdir.bind(null, configDir)
+    }
 
-        return of()
-    })
+    var processDashDir = path.join(configDir, processDashDirectory())
+    var processDashDirStat = yield both(fs.stat.bind(null, processDashDir))
 
-    var pdashFile = pdashDirectory()
-    var pdashDir = path.join(configDir, pdashFile)
-    var pdashDirStat = chain(configDirExists, function () {
-        return fs.stat.bind(null, pdashDir)
-    })
+    if (processDashDirStat[0]) {
+        yield fs.mkdir.bind(null, processDashDir)
+    }
 
-    var pdashDirExists = either(pdashDirStat, function () {
-        return fs.mkdir.bind(null, pdashDir)
-    }, function (stat) {
-        if (stat && !stat.isDirectory()) {
-            console.error("~/.config/" + pdashFile + " directory exists but" +
-                " it is not a directory")
-            return process.exit(1)
-        }
-
-        return of()
-    })
-
-    return map(pdashDirExists, function () {
-        return pdashDir
-    })
+    return processDashDir
 }
 
-function pdashDirectory() {
-    return env.PDASH_DIRECTORY || "process-dash"
+function processDashDirectory() {
+    return env.processDash_DIRECTORY || "process-dash"
 }
 
 function home() {
